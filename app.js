fdd9e4d31090028e116e9a88f5276b5bb11e88c2
HardikSharmaone8 <hardiksharma30018@gmail.com>
2022-10-29 00:16:22 +0530

    remove homepage

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require("./routes/routes"));
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
 //this part of code is described that when we want to use front end part than execute this code otherwise not execute
 
 if (process.env.NODE_ENV === "production") {
